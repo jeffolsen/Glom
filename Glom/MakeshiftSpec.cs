cd56edac4558aeb0bfad75b365f879ac73d9d4f3
jeffolsen <olsenjeffn@gmail.com>
2014-06-20 14:19:35 -0600

    extract test framework as class and add Glom and Glommable classes

---
+++ Glom/MakeshiftSpec.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.IO;
+
+namespace Glom
+{
+    class MakeshiftSpec
+    {
+        string description;
+
+        protected void Expect(bool expectation, [CallerFilePath] string path = "", [CallerLineNumber] int lineNumber = 0)
+        {
+            if (!expectation)
+            {
+                throw new Exception("Failed expectation: " + File.ReadLines(path).Skip(lineNumber - 1).First().Trim());
+            }
+        }
+
+        protected void Describe(string descriptor, Action body)
+        {
+            string oldDescription = description;
+            description += " " + descriptor;
+            body.Invoke();
+            description = oldDescription;
+        }
+
+        protected void It(string descriptor, Action test)
+        {
+            try
+            {
+                test.Invoke();
+                System.Diagnostics.Debug.WriteLine("PASS: It " + descriptor);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine("FAIL: It " + descriptor + " EXCEPTION " + e);
+            }
+        }
+    }
+}
--- Glom/Program.cs
+++ Glom/Program.cs
@@ -3,40 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.CompilerServices;
-using System.IO;
 
 namespace Glom
 {
-    class Program
+    class Program : MakeshiftSpec
     {
-        static void Expect(bool expectation, [CallerFilePath] string path = "", [CallerLineNumber] int lineNumber = 0)
-        {
-            if (!expectation)
-            {
-                throw new Exception("Failed expectation: " + File.ReadLines(path).Skip(lineNumber - 1).First().Trim());
-            }
-        }
-
-        static void It(string descriptor, Action test)
-        {
-            try
-            {
-                test.Invoke();
-                System.Diagnostics.Debug.WriteLine("PASS: It " + descriptor);
-            }
-            catch (Exception e)
-            {
-                System.Diagnostics.Debug.WriteLine("FAIL: It " + descriptor + " EXCEPTION " + e);
-            }
-        }
-
         static void Main(string[] args)
         {
-            It("should fail false expectations", () =>
+            new Program().Test();
+        }
+
+        void Test() {
+            Describe("Glom", () =>
             {
-                Expect(1 == 2); // because i'm god
+                It("should let you add glommables", () =>
+                {
+                    Glom glom = new Glom();
+                    glom.Add(new MockGlommable() { });
+                    Expect(glom.Count == 1);
+                });
             });
+        }
+
+        class MockGlommable : Glommable
+        {
+                
         }
     }
 }
